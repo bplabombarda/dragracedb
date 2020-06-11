ba3f0fcbdb70a5d086c8cf450b84ebf89512df77
Brett LaBombarda <bplabombarda@gmail.com>
2020-06-10 22:10:49 -0500

    django-admin-interface

--- dragracedb/settings.py
+++ dragracedb/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'lip_syncs.apps.LipSyncsConfig',
     'queens.apps.QueensConfig',
     'seasons.apps.SeasonsConfig',
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -128,4 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+
