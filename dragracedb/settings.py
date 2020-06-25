02de16620f2cf3f2126140d8440a71c44b4a695b
Brett LaBombarda <bplabombarda@gmail.com>
2020-06-25 03:36:38 -0500

    Remove SECRET_KEY

--- dragracedb/settings.py
+++ dragracedb/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#e2pb7lu$starlin$09h69hd8)=3g_wcg7%*pa(j8lriarfo00'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
